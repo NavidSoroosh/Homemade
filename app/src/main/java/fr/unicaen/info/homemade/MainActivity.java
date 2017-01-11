200711ec2af2f3691c5cb814653dc41cccbbef89
Charaf <charaf297@gmail.com>
2017-01-11 21:29:12 +0100

    1ère interface et 1er commit
    Hallilujah , l'appareil photo demarre en cliquant dessus ^^

--- app/src/main/java/fr/unicaen/info/homemade/MainActivity.java
+++ app/src/main/java/fr/unicaen/info/homemade/MainActivity.java
@@ -1,13 +1,23 @@
 package fr.unicaen.info.homemade;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "fr.unicaen.info.users.a21612912.homemade.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void cameraStart(View v){
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivityForResult(intent, 0);
+    }
+
+
 }
