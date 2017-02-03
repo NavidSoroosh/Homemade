c9896c969fe6aeccf0e01e7a3de46e812c527ffe
Charaf <charaf297@gmail.com>
2017-02-03 14:46:31 +0100

    load captured image WORKS

--- app/src/main/java/fr/unicaen/info/homemade/MainActivity.java
+++ app/src/main/java/fr/unicaen/info/homemade/MainActivity.java
@@ -1,23 +1,56 @@
 package fr.unicaen.info.homemade;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "fr.unicaen.info.users.a21612912.homemade.MESSAGE";
+    private Uri mFileUri;
+    private final Context mContext = this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     public void cameraStart(View v){
-        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-        startActivityForResult(intent, 0);
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Intent intent = new Intent(MainActivity.this,ShowActivity.class);
+            intent.putExtras(extras);
+            startActivity(intent);
+        }
+    }
+
+   
 
 }
